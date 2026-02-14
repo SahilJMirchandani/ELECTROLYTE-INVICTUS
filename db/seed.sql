a98c64210cd35bba7f6881ac4b596f968701912c
Sahil Mirchandani <sahilmirchandani@gmail.com>
2026-02-14 18:12:17 +0530

    Create seed.sql with Atomberg and Bajaj component data

---
+++ db/seed.sql
@@ -0,0 +1,7 @@
+-- Seed Data for Atomberg and Bajaj components
+
+INSERT INTO components (id, name, company, type, price, specs) VALUES
+(1, 'Atomberg Efficio', 'Atomberg', 'Fan', 3000, '{ "speed": "400 RPM", "power": "28 W" }'),
+(2, 'Atomberg Breeze', 'Atomberg', 'Fan', 2500, '{ "speed": "350 RPM", "power": "28 W" }'),
+(3, 'Bajaj Tornado', 'Bajaj', 'Fan', 2200, '{ "speed": "380 RPM", "power": "50 W" }'),
+(4, 'Bajaj Maxima', 'Bajaj', 'Fan', 2700, '{ "speed": "390 RPM", "power": "50 W" }');
